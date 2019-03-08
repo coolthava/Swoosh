6748718b55bd4ea4ed82725d1f9cf9af52fe858d
thavaj <jtthavaku@astro.com.my>
2019-03-08 18:00:15 +0800

    Lesson 6

--- app/src/main/java/com/thavaj/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/thavaj/swoosh/WelcomeActivity.kt
@@ -3,9 +3,10 @@ package com.thavaj.swoosh
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
